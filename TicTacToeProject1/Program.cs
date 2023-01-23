8e72a29b9f3517d2bbe54cbcd03baae0d2e22c8d
Sydney Hull <sydneyjhull@gmail.com>
2023-01-23 09:02:37 -0700

    trial run

--- TicTacToeProject1/Program.cs
+++ TicTacToeProject1/Program.cs
@@ -7,6 +7,7 @@ namespace TicTacToeProject1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi!");
         }
     }
 }
