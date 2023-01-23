b9c1e26c333972f22481cde7766909669672979a
zirvine <95656693+zirvine@users.noreply.github.com>
2023-01-23 09:06:31 -0700

    Comments

--- TicTacToeProject1/Program.cs
+++ TicTacToeProject1/Program.cs
@@ -7,8 +7,12 @@ namespace TicTacToeProject1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hi!");
-            Console.WriteLine("Llama");
-        }
+            //Welcome the user to the game
+            //• Create a game board array to store the players’ choices
+            //• Ask each player in turn for their choice and update the game board array
+            //• Print the board by calling the method in the supporting class
+            //• Check for a winner by calling the method in the supporting class, and notify the players
+            //when a win has occurred and which player won the game
+    }
     }
 }
