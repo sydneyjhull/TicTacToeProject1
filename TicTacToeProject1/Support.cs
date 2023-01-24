2530acbdc2364edcd16e173bcfe579a06b9201e5
zirvine <95656693+zirvine@users.noreply.github.com>
2023-01-24 11:13:01 -0700

    PrintArray method

--- TicTacToeProject1/Support.cs
+++ TicTacToeProject1/Support.cs
@@ -5,9 +5,23 @@ using System.Text;
 namespace TicTacToeProject1
 {
     class Support
-    {   
+    {
         //Receive the “board” array from the driver class
+
         //Contain a method that prints the board based on the information passed in
+        public void PrintArray(string[] array)
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", array[0], array[1], array[2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", array[3], array[4], array[5]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}  ", array[6], array[7], array[8]);
+            Console.WriteLine("     |     |     ");
+        }
+
         //Contain a method that receives the game board array as input and returns if there is a
         //winner and who it was
     }
