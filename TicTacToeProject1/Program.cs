b8b18a2d53ed5593fe4a292d512e297cf96656b7
Sydney Hull <sydneyjhull@gmail.com>
2023-01-24 11:39:01 -0700

    changes

--- TicTacToeProject1/Program.cs
+++ TicTacToeProject1/Program.cs
@@ -6,6 +6,7 @@ namespace TicTacToeProject1
     {
         static void Main(string[] args)
         {
+            Support sp = new Support();
             //Welcome the user to the game
             Console.WriteLine("Let's play Tic-Tac-Toe!");
 
@@ -13,16 +14,33 @@ namespace TicTacToeProject1
             string[] gameBoard = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
             //• Ask each player in turn for their choice and update the game board array
-            Console.WriteLine("Player 1, where do you want to place an X?:");
-            string placeX = "";
-            placeX = Console.ReadLine();
-
-            Console.WriteLine("Player 2, where do you want to place an O?:");
-            string placeO = "";
-            placeO = Console.ReadLine();
+            if (CheckWin != true)
+            {
+                int choice = 0;
+                int player = 1;
+                if (player % 2 != 0)
+                {
+                    Console.WriteLine("Player 1, where do you want to place an X ?:");
+                    choice = int.Parse(Console.ReadLine());
+                    player++;
+                    gameBoard[choice] = "X";
+                    sp.CheckWin(gameBoard);
+                } 
+                else
+                {
+                    Console.WriteLine("Player 2, where do you want to place an X ?:");
+                    choice = int.Parse(Console.ReadLine());
+                    player++;
+                    gameBoard[choice] = "O";
+                    sp.CheckWin(gameBoard);
+                }
+            }
             //• Print the board by calling the method in the supporting class
+            Console.WriteLine(sp.PrintArray(gameBoard));
+
             //• Check for a winner by calling the method in the supporting class, and notify the players
             //when a win has occurred and which player won the game
+
         }
     }
 }
