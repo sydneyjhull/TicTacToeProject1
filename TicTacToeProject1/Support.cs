859cb2164cb93904a8a64654c428063dd12abf3e
zirvine <95656693+zirvine@users.noreply.github.com>
2023-01-24 10:56:48 -0700

    support comments

--- TicTacToeProject1/Support.cs
+++ TicTacToeProject1/Support.cs
@@ -5,6 +5,10 @@ using System.Text;
 namespace TicTacToeProject1
 {
     class Support
-    {
+    {   
+        //Receive the “board” array from the driver class
+        //Contain a method that prints the board based on the information passed in
+        //Contain a method that receives the game board array as input and returns if there is a
+        //winner and who it was
     }
 }
